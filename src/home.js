df10c07434c59e52e57f0bef69c2bfb939c91f00
Iuri Chiba <iurichiba@Iuris-MacBook-Pro.local>
2021-10-13 23:37:59 -0300

    Unused boilerplate code removed, basic homescreen added

---
+++ src/home.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './home.css';
+
+function Home() {
+  return (
+    <div className="home">
+    <img src={logo} className="logo" />
+    </div>
+    );
+}
+
+export default Home;
